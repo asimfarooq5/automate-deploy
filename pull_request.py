0f2aed093933d77c29e2de712ec3c4797162f833
Asim Farooq <asimfarooq5@gmail.com>
2019-09-23 13:39:00 +0500

    create repo

--- pull_request.py
+++ pull_request.py
@@ -1,14 +1,36 @@
 import os
+import shlex
 import subprocess
 from flask import Flask
 
 app = Flask(__name__)
 
 
+def run(command):
+    subprocess.check_call(shlex.split(command))
+
+
 def pull_and_restart(path, service_name):
     os.chdir(path)
-    subprocess.check_call(['git', 'pull'])
-    subprocess.check_call(['systemctl', 'restart', service_name])
+    run('git pull')
+    run(f"systemctl restart {service_name}")
+
+
+def create_repo(path, repo_name):
+    os.chdir(path)
+    run(f'mkdir {repo_name}')
+    run('git init')
+    run('touch README.md')
+    run('git add README.md')
+    run('git commit -am "initial commit"')
+    run(f'git remote add origin git@github.com:codebasepk/{repo_name}.git')
+    run('git push -u origin master')
+
+
+@app.route('/create_repo', methods=['POST'])
+def created():
+    create_repo('/home/root1/',  'create_test')
+    return 'Ok', 200
 
 
 @app.route('/pull_restart', methods=['POST'])
